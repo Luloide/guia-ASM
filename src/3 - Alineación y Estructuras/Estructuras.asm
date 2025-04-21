d2314cf2a0d365ffad9b45157978a06cb4997543
Luloide <lucia.silva.alberto@gmail.com>
2025-04-21 12:31:28 -0300

    avances

--- src/3 - Alineación y Estructuras/Estructuras.asm
+++ src/3 - Alineación y Estructuras/Estructuras.asm
@@ -4,15 +4,15 @@
 ; Completar las definiciones (serán revisadas por ABI enforcer):
 NODO_OFFSET_NEXT EQU 0
 NODO_OFFSET_CATEGORIA EQU 8
-NODO_OFFSET_ARREGLO EQU 12
-NODO_OFFSET_LONGITUD EQU 20
-NODO_SIZE EQU 24
-PACKED_NODO_OFFSET_NEXT EQU 8
+NODO_OFFSET_ARREGLO EQU 16
+NODO_OFFSET_LONGITUD EQU 24
+NODO_SIZE EQU 32
+PACKED_NODO_OFFSET_NEXT EQU 0
 PACKED_NODO_OFFSET_CATEGORIA EQU 8
-PACKED_NODO_OFFSET_ARREGLO EQU 8
-PACKED_NODO_OFFSET_LONGITUD EQU 8
-PACKED_NODO_SIZE EQU 8
-LISTA_OFFSET_HEAD EQU 8
+PACKED_NODO_OFFSET_ARREGLO EQU 9
+PACKED_NODO_OFFSET_LONGITUD EQU 17
+PACKED_NODO_SIZE EQU 21
+LISTA_OFFSET_HEAD EQU 0
 LISTA_SIZE EQU 8
 PACKED_LISTA_OFFSET_HEAD EQU 8
 PACKED_LISTA_SIZE EQU 8
@@ -32,13 +32,14 @@ global cantidad_total_de_elementos_packed
 ;registros: lista[RDI]
 cantidad_total_de_elementos:
 	mov RAX, 0 ; inicializo en 0 la cantidad de elementos
+	mov RBX, [RDI]; agarro el primer nodo
 	
 .loop:
 	test RDI, [RDI]; chequeo que haya proximo nodo
 	je .terminar;si no es nulo
 	; longitud esta a 12 bytes de distancia desde el inicio
 	add RAX, [RDI + NODO_OFFSET_LONGITUD] ; agrego la longitud a la cantidad de elementos actuales
-	mov RDI, [RDI + NODO_OFFSET_NEXT]; cargo el proximo nodo
+	mov RDI, [RDI]; cargo el proximo nodo
 	jmp .loop
 
 .terminar:
